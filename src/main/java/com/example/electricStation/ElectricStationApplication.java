c6c100dfe7c55875bdc870cdf543e2b629269f0c
YoonKiBum <ykbsuper@naver.com>
2024-08-26 21:22:53 +0900

    feature: initial-commit

---
+++ src/main/java/com/example/electricStation/ElectricStationApplication.java
@@ -0,0 +1,13 @@
+package com.example.electricStation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricStationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectricStationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/electricStation/ElectricStationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.electricStation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectricStationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
